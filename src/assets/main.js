df2e9da4a2374f5bdb0ec4ac05b4fef91b7c6c40
ProgramWen <devenli1225@gmail.com>
2026-07-06 17:19:45 +0800

    fix: 刪除 assets 目錄下殘留的舊進入點

--- src/assets/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue' // 確保這一行路徑是對的
-import router from './router'
-import './assets/main.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
